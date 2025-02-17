a0bdc17111fcaff77f06116b5d3e27490956517b
Mark Solante <marksolante221@gmail.com>
2025-02-17 21:59:22 +0800

    [UPDATE 0.2.8 : Added Scaffolding for Views]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { ComponentsPageComponent } from './views/components-page/components-page.component';
+import { AboutPageComponent } from './views/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"/",
+    component: LandingPageComponent
+  },
+  {
+    path:"/components-page",
+    component: ComponentsPageComponent
+  },
+  {
+    path:"/about-page",
+    component: AboutPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
